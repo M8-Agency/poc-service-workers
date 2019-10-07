9f4b5486fe2e6c970eb2eff900515761da0d8515
Antonio Rodriguez <antoniwan@gmail.com>
2019-10-07 12:22:33 -0400

    project init

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>
+        Proof-of-concept:
+        <br /> Service Worker
+      </h1>
+      <h2>Current Active Features</h2>
+      <ul>
+        <li>Offline-first</li>
+      </ul>
+    </main>
+  );
+}
